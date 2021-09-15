130baf7ebe06109d992da02878f550a387a93ff2
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-09-15 02:45:30 +0000

    sd

---
+++ assignment8-3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int findMin(int [], int, int);
+
+int main()
+{
+  const int N = 10;
+  int numbers[N] = {25, 10, 15,30,35,40,45,55};
+
+  for(int i =0; i<N; i++)
+  {
+    int minidx;
+    minidx= findMin(numbers, N, i); // find the i_smallest number
+    swap(numbers[i], numbers[minidx]);
+
+  }
+}
+int findMin(int numbers[], int N, int ith)
+{
+  int min, minidx, i;
+  min = numbers[ith];
+    for(i=ith ;i<N;i++)
+    {
+      if( min > numbers[i])
+      {
+        min = numbers[i];
+        minidx = i;
+      }
+    }
+    return minidx; //1
+}
+
+void printArray(int a[],int x)
+{
+  for(int i=0; i<x; i++){
+  cout << a[i] << "\t";
+}
+cout << "\n";
+}
