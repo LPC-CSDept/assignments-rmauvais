1ac8557e651a5c5c518545cc0cb53e1e1250be89
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-10-20 05:35:35 +0000

    array

--- quiz2-1.cpp
+++ quiz2-1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+void fillupArray(int *arr)
+{
+  for (int i =0; i<10; i ++)
+  {
+    arr[i]=rand()%100;
+  }
+}
+
+void printArray(int *arr)
+{
+  for
+}
 int main()
 {
-  
+
 }
