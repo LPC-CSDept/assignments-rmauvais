35ab52cadfdebb59038d22b856361010113fb053
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-10-20 05:41:26 +0000

    explanation p2

--- quiz2-1.cpp
+++ quiz2-1.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 using namespace std;
-
+//fill values
 void fillupArray(int *arr)
 {
   for (int i =0; i<10; i ++)
@@ -10,14 +10,21 @@ void fillupArray(int *arr)
     arr[i]=rand()%100; //rand() generates a random int
   }
 }
-
+//print array
 void printArray(int *arr)
 {
   for(int i = 0; i<10; i ++)
   cout << arr[i] << " ";
   return;
 }
+
 int main()
 {
+  int *arr = new int[10]; //make 10 integer spots
+  // fill array randomly
+  fillupArray(arr);
+  // print array again
+  printArray(arr);
 
+  return 0;
 }
