905d601483b2c6ca81f69567b33df53f32458cb0
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-10-20 06:15:08 +0000

    structure

--- quiz2-2write.cpp
+++ quiz2-2write.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+struct student{
+  int rollNo;
+  char name[50];
+  int marks[3];
+  int total;
+};
+
+bool compare(student a, student b){
+  if(a.total<b.total)
+return 1;
+else
+return 0;
+}
+
 int main()
 {
-  
+
 }
