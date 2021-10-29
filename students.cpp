fea5d7b7f32c22927a8d4604ff9f825a697fc643
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-10-29 20:04:40 +0000

    students

--- students.cpp
+++ students.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 
 using namespace std;
+
+class Student {
+  private:
+
+  int sid;
+  string sname;
+  double scores[3];
+
+  public:
+
+    int getSid() const {
+      return sid;
+    }
+    void setSid(int sid){
+      this ->sid = sid;
+    }
+}
