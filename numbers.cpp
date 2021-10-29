eb4cfd745fa3add2f08caea8a2d527948ba0c7ed
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-10-29 05:42:36 +0000

    numbers

--- numbers.cpp
+++ numbers.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 #include<time.h>
+#include "Numbers.h"
 
 using namespace std;
+
+Numbers :: Numbers(){
+  size =10;
+  head = new int[size];
+}
+
+Numbers :: Numbers(int size){this->size=size;
+head=new int[size]
+;}
+
+void Numbers :: swap()
