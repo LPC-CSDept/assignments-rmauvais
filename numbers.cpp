26fd05c6402e23087beb7cb475562237e08fac50
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-10-29 05:44:27 +0000

    numbers

--- numbers.cpp
+++ numbers.cpp
@@ -14,4 +14,15 @@ Numbers :: Numbers(int size){this->size=size;
 head=new int[size]
 ;}
 
-void Numbers :: swap()
+void Numbers :: swap(int *arr, int i, int j){
+  int temp = arr[i];
+  arr[i]=arr[j];
+  arr[j]=temp;
+}
+
+void Numbers :: print(){
+  for(int i =0;i<size;i++)
+  cout<<head[i]<<" ";
+
+  cout <<endl;
+}
