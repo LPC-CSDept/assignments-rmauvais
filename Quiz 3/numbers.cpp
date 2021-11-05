29fdc26467086246806502d534f1999f10793f60
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-05 04:07:26 +0000

    numbers c

---
+++ Quiz 3/numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include "numbers.hpp"
+
+Numbers::Numbers()
+{
+  ID=-1;
+}
+//parameter
+Numbers::Numbers(int id, int n)
+{
+  ID=id;
+  for(int i=0; i<n; i++)
+  {
+    int j = rand()%100;
+    numbers.push_back(j);
+}
+}
