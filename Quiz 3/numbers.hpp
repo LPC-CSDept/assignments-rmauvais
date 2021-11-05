8befd399080336d2264a8c40bb866288496d7357
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-05 04:01:38 +0000

    numbers h

--- Quiz 3/numbers.hpp
+++ Quiz 3/numbers.hpp
@@ -10,12 +10,14 @@ class Numbers
   public:
   Numbers();
   Numbers(int, int);
+  // return ID
   int getID()const;
+//return vector
   int getSize()const;
   int getElm(int)const;
   int getMax()const;
   int getMin()const;
   int getSum()const;
-
+// delete elements that are equal to d
   void deleteEle(int e);
   };
