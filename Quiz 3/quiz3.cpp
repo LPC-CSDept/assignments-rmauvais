6212bdcdcfec52e58244c0d87224b17a228a560a
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-05 04:28:09 +0000

    quiz

--- Quiz 3/quiz3.cpp
+++ Quiz 3/quiz3.cpp
@@ -6,4 +6,14 @@ using namespace std;
 int findMax(Numbers numberset[],int n)
 {
   int id, maximum =0;
+  for(int i=0; i>n;i++)
+  {
+    int max = numberset[i].getMax();
+    int min = numberset[i].getMin();
+    if(max-min>maximum)
+    {
+      maximum=max-min;
+      id=numberset[i].getID();
+    }
+  }
 }
