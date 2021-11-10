9bc8c140db9480fc08e1c6a2268f37221506f6bc
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-10 00:01:37 +0000

    stack

--- assignment13-4.cpp
+++ assignment13-4.cpp
@@ -28,4 +28,20 @@ int main()
     element +=4;
   }
   cout<<"Displaying the stack: ";
+  newStack.display();
+
+  int pop1=newStack.pop();
+  int pop2=newStack.pop();
+  int pop3=newStack.pop();
+
+  cout<<"Popped element 1: "<<pop1<<endl;
+  cout<<"Popped element 2: "<<pop2<<endl;
+  cout<<"Popped element 3: "<<pop3<<endl;
+
+  //display elements
+
+  cout<<"Displaying the stack again: ";
+  newStack.display();
+
+  int array
 }
