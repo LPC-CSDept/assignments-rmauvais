13babd49e27ea804ea876fdcd20aa614973cd77f
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-17 04:19:55 +0000

    14-3

--- assignment14-3.cpp
+++ assignment14-3.cpp
@@ -25,3 +25,45 @@ class Square
   }
   friend class Rect;
 };
+class Rect{
+  private:
+  double width;
+  double height;
+  public:
+  Rect()
+  {
+    width=0;
+    height=0;
+  }
+  void setWidth(double width)
+  {
+    this->width=width;
+  }
+  void setheight(double height)
+  {
+    this->height=height;
+  }
+  double getWidth()const
+  {
+    return width;
+  }
+  double getHeight()const
+  {
+    return height;
+  }
+  double getArea()const{
+    return width*height;
+  }
+  Rect makeRectanglewithSquare(Square s)
+  {
+    Rect *r=new Rect();
+    r->height=height;
+    r->width=(double)s.width;
+    return *r;
+  }
+};
+
+int main()
+{
+  
+}
