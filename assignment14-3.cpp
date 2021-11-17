ce0b79693825998f42645357f3126b802c8d9297
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-17 04:07:56 +0000

    14-3

--- assignment14-3.cpp
+++ assignment14-3.cpp
@@ -5,10 +5,23 @@ using namespace std;
 class Square
 {
   private: 
-  int width;
+      int width;
   public:
   Square()
   {
     width =0;
   }
-}
+  void setWidth(int width)
+  {
+    this->width=width;
+  }
+  int getWidth()
+  {
+    return width;
+  }
+  int getArea()
+  {
+    return width*width;
+  }
+  friend class Rect;
+};
