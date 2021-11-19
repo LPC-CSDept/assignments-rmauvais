8314af39e969ed25f0b453625bbe4a2995ba97eb
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-19 05:14:40 +0000

    14-4

--- assignment14-4.cpp
+++ assignment14-4.cpp
@@ -26,3 +26,17 @@ class Numbers
 };
 
 Numbers::Numbers()
+{
+  size = s;
+  numbers = new int[s];
+}
+
+Numbers::Numbers(const Numbers &num)
+{
+  size = num.size;
+  numbers = new int[size];
+  for(int i=0;i<size;++i)
+  {
+    numbers[i]=num.numbers[i];
+  }
+}
