5d5f057179d14e84245bb463c3dcd0bcd71e4200
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-24 04:33:36 +0000

    14-5

--- assignment14-5.cpp
+++ assignment14-5.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
+#include "Numbers.hpp"
 
 using namespace std;
+
+int main()
+{
+  Numbers n1(10);
+  Numbers n2;
+  n2=n1;
+  cout<<"Display n1: ";
+  printNumbers(n1);
+  cout<<"\nDisplay n2: ";
+  printNumbers(n2);
+  cout<<"\nn1>n2=";
+  cout<<(n1>n2)<<endl;
+  Numbers n3 =n1+n2;
+  cout<<"\nDisplay n3: ";
+  printNumbers(n3);
+  return 0;
+}
