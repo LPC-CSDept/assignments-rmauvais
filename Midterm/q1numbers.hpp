d58b7c0da5afaaca29c9063222edd1e56f72064e
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-28 19:04:12 +0000

    if else return

--- Midterm/q1numbers.hpp
+++ Midterm/q1numbers.hpp
@@ -46,7 +46,19 @@ boolean operator >const Numbers &n2)
   int sum1 =0, sum2=0;
   //object n1
   for (int i=0; i<values.size();i++)
+  //sum of 1
+    sum1 +=values[i];
   //object n2
-  
-}
-}
+  for (int i=0; i<n2.values.size();i++)
+  //sum of 2
+    sum2 +=n2.values[i];
+
+//if return1 is true n1 is greater than n2
+  if(sum1>sum2)
+    return true;
+  else
+// otherwise false
+    return false;
+  }
+};
+
