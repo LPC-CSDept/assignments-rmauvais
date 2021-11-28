1a82ca9b9b8b141e26460eca65b3725c6c82bd2d
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-28 20:00:44 +0000

    inputs outputs

--- Midterm/q2midterm.cpp
+++ Midterm/q2midterm.cpp
@@ -8,8 +8,25 @@ int main()
   int n =10;
   //student class object
   student a[10];
-  //take the input
+  //take users inputs
   for(int i=0;i<n;i++){
+    //input Student ID
     cout<<"Enter Student ID: ";
+    cin>>a[i].ID;
+    //input Student Name
+    cout<<"Enter Student Name: ";
+    cin>>a[i].name;
+    //input Score 1
+    cout<<"Enter Score 1: ";
+    cin>>a[i].score1;
+    //input Score 2
+    cout<<"Enter Score 2: ";
+    cin>>a[i].score2;
+    //input Score 3
+    cout<<"Enter Score 3: ";
+    cin>>a[i].score3;
+    // calculate all the scores by adding them up
+    a[i].total_scores=a[i].scores1+a[i].score2+a[i].score3;
   }
+
 }
