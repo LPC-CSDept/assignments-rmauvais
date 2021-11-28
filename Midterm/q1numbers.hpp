5e7d838590e5fb951652882a21ed6d673a8b0a94
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-28 18:55:07 +0000

    operators

--- Midterm/q1numbers.hpp
+++ Midterm/q1numbers.hpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//create class
 class Numbers
 {
   private:
@@ -20,7 +21,25 @@ Numbers(int i, int n)
   ID=i;
   for(i=0;i<n;i++)
   {
-    int num = (rand())
+    //create the random number generator
+    int num = (rand()%(99=0+1))+0;
+    values.push_back(num);
   }
+  numOfObjects++;
 }
+//retrieve ID
+int getID()
+{
+  //return the ID
+  return ID;
+}
+static int getNumObjects()
+{
+  // return getNumObjects
+  return numOfObjects;
+}
+
+friend void printNumbers(Numbers n);
+
+boolean operator >const Numbers &n2)
 }
