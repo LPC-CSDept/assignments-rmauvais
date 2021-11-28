bfc353e4002bf0613afe6407692a7b2227630247
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-11-28 19:56:38 +0000

    input

--- Midterm/q2midterm.cpp
+++ Midterm/q2midterm.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-  
+  int n =10;
+  //student class object
+  student a[10];
+  //take the input
+  for(int i=0;i<n;i++){
+    cout<<"Enter Student ID: ";
+  }
 }
