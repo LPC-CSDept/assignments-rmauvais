302940875918dbcb3cf1bc2c91f532d40fe8996b
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-10 06:11:14 +0000

    run

--- assignment19-5.cpp
+++ assignment19-5.cpp
@@ -7,6 +7,7 @@ void swap(int* c, int* k)
   *k = g;
 }
 
+
 int dividing (int A[], int low_index, int high_index) 
 {
 int element_pivo=A[high_index];
@@ -20,8 +21,29 @@ for(int c=low_index; c <=high_index-1; c++)
     swap(&A[z],&A[high_index]);
   }
 }
+swap(&A[z+1],&A[high_index]);
+return (z+1);
 }
 
+//sort
+void sort_quick(int A[], int low_index, int high_index)
+{
+  if(low_index<high_index)
+  {
+    int pivo = dividing(A, low_index, high_index);
+    sort_quick(A, low_index, pivo - 1);
+    sort_quick(A, pivo +1, high_index);
+  }
+}
+
+//array
+void array_print(int A[], int T_size)
+{
+  int z;
+  for(z=0; z<T_size; z++)
+  printf("%d ", A[z]);
+  printf("\n");
+}
 
 // main function
 int main()
