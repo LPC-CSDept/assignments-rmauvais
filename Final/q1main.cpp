51d84f5018eaf4bb194e158e63e6adfd5e5bc027
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-15 06:11:38 +0000

    txt

---
+++ Final/q1main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "q1course.hpp"
+
+using namespace std;
+
+//set binary search
+int binary_search(Course[], int, int);
+int recursive_binary_search(Course[], int, int ,int);
+
+//main file
+int main()
+{
+  //open text file
+ifstream data_file("input_data.txt");
+}
