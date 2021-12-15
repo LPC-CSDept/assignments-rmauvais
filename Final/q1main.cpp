2dd5c21b4419787af05805badbaf3ffcf40500ac
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-15 06:17:45 +0000

    main file

--- Final/q1main.cpp
+++ Final/q1main.cpp
@@ -11,5 +11,13 @@ int recursive_binary_search(Course[], int, int ,int);
 int main()
 {
   //open text file
-ifstream data_file("input_data.txt");
+ifstream data_file("q1data.txt");
+
+if (data_file.fail()){
+  cout << "Error! Failed to open q1data.txt file." << endl;
+  cout << "Make sure q1data.txt feil is present in the current working directory." << endl;
+  return 1;
+}
+
+// declare array of 10 objects
 }
