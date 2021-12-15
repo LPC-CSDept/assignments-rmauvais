ac6c7383b1ec6b6888c76f6c6c60e052745f547d
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-15 06:30:49 +0000

    creating object

--- Final/q1main.cpp
+++ Final/q1main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-//set binary search
+//set the binary search
 int binary_search(Course[], int, int);
 int recursive_binary_search(Course[], int, int ,int);
 
@@ -21,5 +21,39 @@ if (data_file.fail()){
 }
 
 // declare array of 10 objects
+Course courses[10];
+int n =0;
+
+//read file
+while(!data_file.eof()){
+  int id, credit;
+  string name;
+
+  //read id
+  data_file>>id;
+  //read name
+  data_file>>name;
+  //read credit
+  data_file>>credit;
+
+  //create object
+  Course c(id, name, credit);
+
+  //store object into array
+  courses[n++]=c;
+}
+
+//close input data file
+data_file.close();
+
+//sort course arrays by id's
+// use sorting algorithm
+for (int p=0; p<n; p++){
+  for (int q=0; q<n-p-1; q++){
+    if (courses[q].getid()> courses[q+1].getid()){
+      
+    }
+  }
+}
 
 }
