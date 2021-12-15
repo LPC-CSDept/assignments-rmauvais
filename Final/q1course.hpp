bc1929f430a9592ff192bb496e12760cb77367c2
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-15 05:24:32 +0000

    q1

---
+++ Final/q1course.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class Course{
+  private: 
+  int id; //store course id
+  string name; // store course name
+  int credit; // store course credit
+
+  public:
+  Course(); 
+}
