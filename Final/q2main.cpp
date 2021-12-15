b77e04c0171e3045568d23c83a51ac035b55c971
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-15 22:56:00 +0000

    if failed

--- Final/q2main.cpp
+++ Final/q2main.cpp
@@ -6,3 +6,20 @@ using namespace std;
 
 //sort function
 void quick_sort(Course *, int, int);
+
+int main()
+{
+  // open text file again from question 1
+  ifstream data_file("q1data.txt");
+
+  //if file is failed to open
+  if (data_file.fail()){
+    cout << "Error! File failed to open, q1data.txt." << endl;
+    cout << "Make sure the q1data.txt file is present in the directory!" << endl;
+    return 1;
+  }
+
+  //use array to hold the 10 objects
+  Course courses[10];
+  
+}
