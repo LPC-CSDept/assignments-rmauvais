b9d9c79746d4b0ce24191ac18f25fa4f91cbc148
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-16 06:08:09 +0000

    fixing

--- Final/q3employeeproductworker.hpp
+++ Final/q3employeeproductworker.hpp
@@ -5,8 +5,31 @@ using namespace std;
 
 class Employee
 {
-  private:
   //store data members
   //store names numbers and hire dates
-  
+  string employee_name;
+  int number;
+  string hire_date;
+  public:
+  string getEmployee_name()
+  {
+    return employee_name;
+  }
+  int getNumber()
+  {
+    return number;
+  }
+  string getHire_date()
+  {
+    return hire_date;
+  }
+  void setEmployee_name(string employee_name)
+  {
+    this->employee_name = employee_name;
+  }
+  void setNumber(int number)
+  {
+    this->number = number;
+
+  }
 }
