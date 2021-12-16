a3a77d0f3d4f305565ae877f29cbf7df18ad7df5
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-16 06:41:10 +0000

    max index

--- Final/q3main.cpp
+++ Final/q3main.cpp
@@ -22,6 +22,21 @@ fin.open("q3data.txt");
 cout<<"Input File\n";
 for(int i=0;i<10;++i)
 {
-  
+  fin>>employee_name;
+  fin>>number;
+  fin>>hire_date;
+  fin>>shift;
+  fin>>hourly_pay_rate;
+  cout<<employee_name<<" "<<number<<" "<<hire_date<<" "<<shift<<" "<<hourly_pay_rate<<endl;
+  p[i] = ProductionWorker(employee_name, number, hire_date, shift, hourly_pay_rate);
 }
+cout<<endl;
+//create the max index variable to find max value of txt file
+int max_index=0;
+for(int i=0;i<10;++i)
+{
+  if(p[i]>p[max_index])
+  max_index =i;
+}
+
 }
