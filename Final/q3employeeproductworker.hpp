a0f6ad86f716b3aad67b12751f09b936e379df86
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-16 06:10:37 +0000

    return hire date

--- Final/q3employeeproductworker.hpp
+++ Final/q3employeeproductworker.hpp
@@ -10,15 +10,21 @@ class Employee
   string employee_name;
   int number;
   string hire_date;
+
+  //default constructor
   public:
+
+  //return employee_name
   string getEmployee_name()
   {
     return employee_name;
   }
+  //return getNumber
   int getNumber()
   {
     return number;
   }
+  //return hire_date
   string getHire_date()
   {
     return hire_date;
@@ -30,6 +36,10 @@ class Employee
   void setNumber(int number)
   {
     this->number = number;
-
   }
-}
+  //return hire_date
+  void setHire_date(string hire_date)
+  {
+    this->hire_date = hire_date;
+  }
+};
