396ade4e294694934043a423f2094543192a0bbd
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-16 05:42:45 +0000

    public private

--- Final/q3employeeproductworker.hpp
+++ Final/q3employeeproductworker.hpp
@@ -1,1 +1,21 @@
 #include <iostream>
+
+using namespace std;
+
+class Employee
+{
+  private:
+  //store data members
+  //store names numbers and hire dates
+  string employee_name, employee_number;
+  string hire_date;
+
+  public:
+  
+  Employee(){
+    // set default constructors with function members
+    employee_name="";
+    employee_number="";
+    hire_date="";
+  }
+}
