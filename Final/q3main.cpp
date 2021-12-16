245033bc4ab40712362a352ca517d87056a93a3b
rmauvais <77745111+rmauvais@users.noreply.github.com>
2021-12-16 06:31:15 +0000

    basics

---
+++ Final/q3main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "q3employeeproductworker.hpp"
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+//declare class array to store all 10 records of text file
+ProductionWorker p[10];
+//initialize variables to read from text file
+string employee_name;
+int number;
+string hire_date;
+int shift;
+double hourly_pay_rate;
+
+
+}
